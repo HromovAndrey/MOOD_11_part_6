e4340c28843f33033a6461025a492e75ebfa07e3
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-04-07 21:07:25 +0300

    практична 2

--- Практична роб мод 11 чвсть 6.py
+++ Практична роб мод 11 чвсть 6.py
@@ -1,34 +1,25 @@
-# Завдання 1
-# Використовуючи поняття множинного успадкування,
-# створіть клас «Коло, поміщене в квадрат».
+# Завдання 2
+# Використовуючи механізм множинного успадкування, створіть клас «Автомобіль». Також мають бути класи:
+# «Колеса», «Двигун», «Двері» та ін
+class Wheels:
+    def __init__(self, number_of_wheels):
+        self.number_of_wheels = number_of_wheels
 
-class Circle:
-    def __init__(self, radius):
-        self.radius = radius
+class Engine:
+    def __init__(self, horsepower):
+        self.horsepower = horsepower
 
-    def area(self):
-        return 3.14 * self.radius * self.radius
+class Doors:
+    def __init__(self, number_of_doors):
+        self.number_of_doors = number_of_doors
 
+class Car(Wheels, Engine, Doors):
+    def __init__(self, number_of_wheels, horsepower, number_of_doors):
+        Wheels.__init__(self, number_of_wheels)
+        Engine.__init__(self, horsepower)
+        Doors.__init__(self, number_of_doors)
 
-class Square:
-    def __init__(self, side):
-        self.side = side
-
-    def area(self):
-        return self.side * self.side
-
-
-class CircleInSquare(Circle, Square):
-    def __init__(self, radius):
-        Square.__init__(self, radius * 2)
-        Circle.__init__(self, radius)
-
-    def remaining_area(self):
-        return self.area() - Square.area(self)
-
-
-radius = 5
-circle_in_square = CircleInSquare(radius)
-print("Площа круга:", circle_in_square.area())
-print("Площа квадрата:", circle_in_square.area())
-
+car = Car(number_of_wheels=4, horsepower=200, number_of_doors=4)
+print("Кількість коліс:", car.number_of_wheels)
+print("Потужність двигуна:", car.horsepower)
+print("Кількість дверей:", car.number_of_doors)

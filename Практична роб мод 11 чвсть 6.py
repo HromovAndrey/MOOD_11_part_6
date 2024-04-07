399f9272d9789d5211e95ff8aea0f2c9ed2b3b61
HromovAndrey <andrey.gromov.36912@gmail.com>
2024-04-07 21:00:45 +0300

    практична 1

--- Практична роб мод 11 чвсть 6.py
+++ Практична роб мод 11 чвсть 6.py
@@ -1,1 +1,34 @@
-Clls comit
+# Завдання 1
+# Використовуючи поняття множинного успадкування,
+# створіть клас «Коло, поміщене в квадрат».
+
+class Circle:
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+
+
+class Square:
+    def __init__(self, side):
+        self.side = side
+
+    def area(self):
+        return self.side * self.side
+
+
+class CircleInSquare(Circle, Square):
+    def __init__(self, radius):
+        Square.__init__(self, radius * 2)
+        Circle.__init__(self, radius)
+
+    def remaining_area(self):
+        return self.area() - Square.area(self)
+
+
+radius = 5
+circle_in_square = CircleInSquare(radius)
+print("Площа круга:", circle_in_square.area())
+print("Площа квадрата:", circle_in_square.area())
+
